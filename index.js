8f4f6549b1d6d773454b15307dacba5f8bef6719
BurnedToaster <128144129+BurnedToaster@users.noreply.github.com>
2023-12-19 12:07:54 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll('#background-slider .slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide); // Show the first slide initially
+setInterval(nextSlide, 3000); // Change slide every 5 seconds
